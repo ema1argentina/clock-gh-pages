b9006229a4b026b549016c6eae506f85604a2ae8
unknown <affatatiej@gmail.com>
2022-04-21 20:03:21 -0500

    Updates

---
+++ precache-manifest.ca476737e48fbb983e7b86845acaedf4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0e376467e20793282c11cf8543eda7b",
+    "url": "/clock-gh-pages/index.html"
+  },
+  {
+    "revision": "bb4d0157f8d4379ec6a7",
+    "url": "/clock-gh-pages/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9f54b42d0d4582292fd6",
+    "url": "/clock-gh-pages/static/js/2.43563c96.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clock-gh-pages/static/js/2.43563c96.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bb4d0157f8d4379ec6a7",
+    "url": "/clock-gh-pages/static/js/main.6ff9ce40.chunk.js"
+  },
+  {
+    "revision": "8cb786b1d8cc5fa8f1ab",
+    "url": "/clock-gh-pages/static/js/runtime-main.60201052.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clock-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
